89ff7638376fad7e2d26a2c634739627462bd29a
Semen <semgenomsk@yandex.ru>
2023-10-05 21:35:39 +0600

    add homW_sem3

---
+++ homW/homW_sem3/test_1.py
@@ -0,0 +1,26 @@
+import yaml
+from testpage import OperationsHelper
+import logging
+
+with open("testdata.yaml") as f:
+    testdata = yaml.safe_load(f)
+
+
+def test_login_positive(browser):
+    logging.info("Test login_positive Starting")
+    testpage = OperationsHelper(browser)
+    testpage.go_to_site()
+    testpage.enter_login(testdata["login"])
+    testpage.enter_pass(testdata["pswd"])
+    testpage.click_login_button()
+    assert testpage.login_success() == f"Hello, {testdata['login']}", "test_login_positive FAILED"
+
+
+def test_contact_us(browser):
+    logging.info("Test add_post Starting")
+    testpage = OperationsHelper(browser)
+    # testpage.go_to_site()
+    # testpage.enter_login(testdata["login"])
+    # testpage.enter_pass(testdata["pswd"])
+    # testpage.click_login_button()
+    testpage.click_contact_button()
